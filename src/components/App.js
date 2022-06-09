12eb6172471f3dea986383702a7a1afe02d15974
WilliamDenega <williamdenega@gmail.com>
2022-06-09 16:26:54 -0400

    added NavBar

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Route , Switch} from 'react-router-dom'
+import NavBar from './NavBar';
+import Home from './Home'
+import SecondPage from './SecondPage';
+import ThirdPage from './ThirdPage'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/page2'>
+          <SecondPage/>
+        </Route>
+        <Route exact path='/page3'>
+          <ThirdPage/>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const linkStyles = {
+    display: "inline-block",
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
+
+ function NavBar() {
+  return (
+    <div className="navbar">
+        <NavLink
+        to='/'
+        exact
+        style={linkStyles}
+        activeStyle={{
+            background: "darkblue",
+        }}
+        >Home
+        </NavLink>
+        <NavLink
+            to= '/page2'
+            exact
+            style={linkStyles}
+            activeStyle={{
+                background:'darkblue'
+            }}
+        >PAGE 2
+        </NavLink>
+        <NavLink 
+            to='/page3'
+            exact
+            style={linkStyles}
+            activeStyle={{
+                background: 'darkblue'
+            }}
+        >PAGE 3
+        </NavLink>
+    </div>
+  )
+}
+export default NavBar
